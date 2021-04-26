f7a789bec1cfe352c9270e855dd3c37a9e039fd0
Khắc Duyệt <duyetbuikhac@gmail.com>
2021-04-26 21:36:00 +0700

    commit -5
    -5

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -21,7 +21,7 @@ class Product extends Model
     // protected $primaryKey = 'id';
     // public $timestamps = false;
     protected $guarded = ['id'];
-    // protected $fillable = [];
+    // protected $fillable = ['name', 'price', 'sale_price', 'cat_id', 'descriptions', 'author', 'publisher', 'size', 'page', 'status'];
     // protected $hidden = [];
     // protected $dates = [];
 
@@ -31,52 +31,36 @@ class Product extends Model
     |--------------------------------------------------------------------------
     */
 
-    public function setImageAttribute($value)
+    protected $casts = [
+        'photos' => 'array'
+    ];
+
+    public function setPhotosAttribute($value)
     {
-        $attribute_name = "image";
-        // or use your own disk, defined in config/filesystems.php
-        $disk = config('backpack.base.root_disk_name');
-        // destination path relative to the disk above
-        $destination_path = "public/uploads/folder_1/folder_2";
+        $attribute_name = "photos";
+        $disk = "public";
+        $destination_path = "uploads";
 
-        // if the image was erased
-        if ($value == null) {
-            // delete the image from disk
-            \Storage::disk($disk)->delete($this->{$attribute_name});
-
-            // set null in the database column
-            $this->attributes[$attribute_name] = null;
-        }
-
-        // if a base64 was sent, store it in the db
-        if (Str::startsWith($value, 'data:image')) {
-            // 0. Make the image
-            $image = \Image::make($value)->encode('jpg', 90);
-
-            // 1. Generate a filename.
-            $filename = md5($value . time()) . '.jpg';
-
-            // 2. Store the image on disk.
-            \Storage::disk($disk)->put($destination_path . '/' . $filename, $image->stream());
-
-            // 3. Delete the previous image, if there was one.
-            \Storage::disk($disk)->delete($this->{$attribute_name});
-
-            // 4. Save the public path to the database
-            // but first, remove "public/" from the path, since we're pointing to it 
-            // from the root folder; that way, what gets saved in the db
-            // is the public URL (everything that comes after the domain name)
-            $public_destination_path = Str::replaceFirst('public/', '', $destination_path);
-            $this->attributes[$attribute_name] = $public_destination_path . '/' . $filename;
-        }
+        $this->uploadMultipleFilesToDisk($value, $attribute_name, $disk, $destination_path);
     }
-
 
     use \Backpack\CRUD\app\Http\Controllers\Operations\FetchOperation;
 
     public function fetchCategory()
     {
         return $this->fetch(\App\Models\Category::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+        static::deleting(function($obj) {
+            if (count((array)$obj->photos)) {
+                foreach ($obj->photos as $file_path) {
+                    \Storage::disk('public')->delete($file_path);
+                }
+            }
+        });
     }
     /*
     |--------------------------------------------------------------------------
